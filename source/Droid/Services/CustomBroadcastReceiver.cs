cae0c673c9e62badff4cb6a2c541d8cb30d49bc5
Roberto Cervantes <rcervantes@outlook.com>
2017-10-20 00:15:51 -0500

    Disaster recovery :)

--- source/Droid/Services/CustomBroadcastReceiver.cs
+++ source/Droid/Services/CustomBroadcastReceiver.cs
@@ -9,23 +9,6 @@ using Gcm.Client;
 //GET_ACCOUNTS is only needed for android versions 4.0.3 and below
 [assembly: UsesPermission(Name = "android.permission.GET_ACCOUNTS")]
 [assembly: UsesPermission(Name = "android.permission.INTERNET")]
-<<<<<<< HEAD:source/Droid/Services/CustomBroadcastReceiver.cs
-[assembly: UsesPermission(Name = "android.permission.WAKE_LOCK")]
-namespace Pharmacy.Droid
-{
-    [BroadcastReceiver(Permission = Gcm.Client.Constants.PERMISSION_GCM_INTENTS)]
-    [IntentFilter(new string[] { Gcm.Client.Constants.INTENT_FROM_GCM_MESSAGE },
-         Categories = new string[] { "@PACKAGE_NAME@" })]
-    [IntentFilter(new string[] { Gcm.Client.Constants.INTENT_FROM_GCM_REGISTRATION_CALLBACK },
-         Categories = new string[] { "@PACKAGE_NAME@" })]
-    [IntentFilter(new string[] { Gcm.Client.Constants.INTENT_FROM_GCM_LIBRARY_RETRY },
-         Categories = new string[] { "@PACKAGE_NAME@" })]
-    public class CustomBroadcastReceiver : GcmBroadcastReceiverBase<PushHandlerService>
-    {
-        // Set the Google app ID.
-        public static string[] senderIDs = new string[] { Constants.FirebaseAppSenderID };
-    }
-=======
 [assembly: UsesPermission(Name = "android.permission.WAKE_LOCK")]
 namespace Pharmacy.Droid
 {
@@ -41,5 +24,4 @@ namespace Pharmacy.Droid
         // Set the Google app ID.
         public static string[] senderIDs = new string[] { Settings.FirebaseAppSenderId };
     }
->>>>>>> ef53c1eded002c1e123b3064dac47327cf12371d:Pharmacy/Droid/Services/CustomBroadcastReceiver.cs
 }
